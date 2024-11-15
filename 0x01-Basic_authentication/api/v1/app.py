1ba78d0ce42a0f6ce99325e16d671d45d92a29fe
adefemi <hardeyfehmie24@gmail.com>
2024-11-15 10:12:23 +0100

    hjd

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -20,6 +20,17 @@ def not_found(error) -> str:
     """
     return jsonify({"error": "Not found"}), 404
 
+@app.errorhandler(401)
+def unauthorized(error):
+    """ 404 Error
+    ---
+    responses:
+      404:
+        description: a resource was not found
+    """
+    return make_response(jsonify({"error": "Unauthorized"}), 401)
+                                 )
+
 
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
